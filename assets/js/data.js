2651ead2137e3cf4d68f05b88d9e05aa6a6c00a7
Paul PLAYE <paul.playe@seloger.com>
2019-08-20 19:55:28 +0200

    Adds fake Videogames streams.

--- assets/js/data.js
+++ assets/js/data.js
@@ -19,286 +19,260 @@ const Data = {
     `Y a des lootboxes ?`,
     `J'ai trouvé un jeu fait par <em>::name::</em> dans les poubelles de Steam !`,
     `Quand je pense qu'on aurait pu apprendre à jouer du piano au lieu de perdre notre temps là dessus…`,
-    `C'est à mettre dans le seau à caca de Noël Malware`,
+    `C'est à mettre dans le seau à caca de Noël Malware`
   ],
   animals: [
     {
-      name: 'bonobo',
-      gender: 1,
+      name: "bonobo",
+      gender: 1
     },
     {
-      name: 'lapin',
-      gender: 1,
+      name: "lapin",
+      gender: 1
     },
     {
-      name: 'canard',
-      gender: 1,
+      name: "canard",
+      gender: 1
     },
     {
-      name: 'chaton',
-      gender: 1,
+      name: "chaton",
+      gender: 1
     },
     {
-      name: 'poney',
-      gender: 1,
+      name: "poney",
+      gender: 1
     },
     {
-      name: 'panda',
-      gender: 1,
+      name: "panda",
+      gender: 1
     },
     {
-      name: 'chimpanzé',
-      gender: 1,
+      name: "chimpanzé",
+      gender: 1
     },
     {
-      name: 'moineau',
-      gender: 1,
+      name: "moineau",
+      gender: 1
     },
     {
-      name: 'caniche',
-      gender: 1,
+      name: "caniche",
+      gender: 1
     },
     {
-      name: 'lama',
-      gender: 1,
+      name: "lama",
+      gender: 1
     },
     {
-      name: 'mouton',
-      gender: 1,
+      name: "mouton",
+      gender: 1
     },
     {
-      name: 'raton',
-      gender: 1,
+      name: "raton",
+      gender: 1
     },
     {
-      name: 'blaireau',
-      gender: 1,
+      name: "blaireau",
+      gender: 1
     },
     {
-      name: 'dauphin',
-      gender: 1,
+      name: "dauphin",
+      gender: 1
     },
     {
-      name: 'ours',
-      gender: 1,
+      name: "ours",
+      gender: 1
     },
     {
-      name: 'puma',
-      gender: 1,
+      name: "puma",
+      gender: 1
     },
     {
-      name: 'lombric',
-      gender: 1,
+      name: "lombric",
+      gender: 1
     },
     {
-      name: 'souris',
-      gender: 0,
+      name: "souris",
+      gender: 0
     },
     {
-      name: 'baleine',
-      gender: 0,
+      name: "baleine",
+      gender: 0
     },
     {
-      name: 'pie',
-      gender: 0,
+      name: "pie",
+      gender: 0
     },
     {
-      name: 'baudroie',
-      gender: 0,
+      name: "baudroie",
+      gender: 0
     },
     {
-      name: 'lapine',
-      gender: 0,
+      name: "lapine",
+      gender: 0
     },
     {
-      name: 'vache',
-      gender: 0,
+      name: "vache",
+      gender: 0
     },
     {
-      name: 'brebis',
-      gender: 0,
+      name: "brebis",
+      gender: 0
     },
     {
-      name: 'antilope',
-      gender: 0,
+      name: "antilope",
+      gender: 0
     },
     {
-      name: 'lionne',
-      gender: 0,
+      name: "lionne",
+      gender: 0
     },
     {
-      name: 'renarde',
-      gender: 0,
+      name: "renarde",
+      gender: 0
     },
     {
-      name: 'guenon',
-      gender: 0,
+      name: "guenon",
+      gender: 0
     },
     {
-      name: 'licorne',
-      gender: 0,
+      name: "licorne",
+      gender: 0
     },
     {
-      name: 'abeille',
-      gender: 0,
+      name: "abeille",
+      gender: 0
     },
     {
-      name: 'guêpe',
-      gender: 0,
+      name: "guêpe",
+      gender: 0
     },
     {
-      name: 'araignée',
-      gender: 0,
+      name: "araignée",
+      gender: 0
     },
     {
-      name: 'fourmi',
-      gender: 0,
+      name: "fourmi",
+      gender: 0
     },
     {
-      name: 'belette',
-      gender: 0,
+      name: "belette",
+      gender: 0
     }
   ],
   adjectives: [
-    [
-      'adipeuse',
-      'adipeux',
+    ["adipeuse", "adipeux"],
+    ["fringante", "fringant"],
+    ["vaillante", "vaillant"],
+    ["excitée", "excité"],
+    ["lubrique", "lubrique"],
+    ["sournoise", "sournois"],
+    ["dylseixque", "dyslexiuqe"],
+    ["perdue", "perdu"],
+    ["endormie", "endormi"],
+    ["silencieuse", "silencieux"],
+    ["infernale", "infernal"],
+    ["peureuse", "peureux"],
+    ["parisienne", "parisien"],
+    ["libidineuse", "libidineux"],
+    ["fofolle", "foufou"],
+    ["gentille", "gentil"],
+    ["sirupeuse", "sirupeux"],
+    ["grassouillette", "grassouillet"],
+    ["pédante", "pédant"],
+    ["sapiosexuelle", "sapiosexuel"],
+    ["ballonnée", "ballonné"],
+    ["constipée", "constipé"],
+    ["turbulente", "turbulent"],
+    ["masculine", "féminin"],
+    ["soyeuse", "soyeux"],
+    ["boiteuse", "boiteux"],
+    ["résiliente", "résilient"],
+    ["primée", "primé"],
+    ["perverse", "pervers"],
+    ["illuminée", "illuminé"],
+    ["violente", "violent"],
+    ["rappeuse", "rappeur"],
+    ["gothique", "gothique"],
+    ["idiote", "idiot"],
+    ["limitée", "limité"]
+  ],
+  videogames: {
+    prefix: [
+      `Tom Clancy's`,
+      `Madden`,
+      `Sid Meier's`,
+      `Player Unknown's`,
+      `Super`,
+      `Call of`,
+      `World of`,
+      `Lea Passion`,
+      `Plants versus`,
+      `Panzer`,
+      `Heart of`,
+      `Final`
     ],
-    [
-      'fringante',
-      'fringant',
+    licence: [
+      `Rainbow Six`,
+      `Counter Strike`,
+      `Civilization`,
+      `Fortnite`,
+      `FIFA`,
+      `Cookie Clicker`,
+      `Star Citizen`,
+      `Diablo`,
+      `Overwatch`,
+      `Tractopelle`,
+      `Camion Poubelle`,
+      `Starcraft`,
+      `League of Legends`,
+      `Just Dance`,
+      `Smash Bros`,
+      `Mount&Blade`,
+      `DragonQuest`,
+      `DOTA`,
+      `Portal`,
+      `Team`
     ],
-    [
-      'vaillante',
-      'vaillant',
-    ],
-    [
-      'excitée',
-      'excité',
-    ],
-    [
-      'lubrique',
-      'lubrique',
-    ],
-    [
-      'sournoise',
-      'sournois',
-    ],
-    [
-      'dylseixque',
-      'dyslexiuqe',
-    ],
-    [
-      'perdue',
-      'perdu',
-    ],
-    [
-      'endormie',
-      'endormi',
-    ],
-    [
-      'silencieuse',
-      'silencieux',
-    ],
-    [
-      'infernale',
-      'infernal',
-    ],
-    [
-      'peureuse',
-      'peureux',
-    ],
-    [
-      'parisienne',
-      'parisien',
-    ],
-    [
-      'libidineuse',
-      'libidineux',
-    ],
-    [
-      'fofolle',
-      'foufou',
-    ],
-    [
-      'gentille',
-      'gentil',
-    ],
-    [
-      'sirupeuse',
-      'sirupeux',
-    ],
-    [
-      'grassouillette',
-      'grassouillet',
-    ],
-    [
-      'pédante',
-      'pédant',
-    ],
-    [
-      'sapiosexuelle',
-      'sapiosexuel',
-    ],
-    [
-      'ballonnée',
-      'ballonné',
-    ],
-    [
-      'constipée',
-      'constipé',
-    ],
-    [
-      'turbulente',
-      'turbulent',
-    ],
-    [
-      'masculine',
-      'féminin',
-    ],
-    [
-      'soyeuse',
-      'soyeux',
-    ],
-    [
-      'boiteuse',
-      'boiteux',
-    ],
-    [
-      'résiliente',
-      'résilient',
-    ],
-    [
-      'primée',
-      'primé',
-    ],
-    [
-      'perverse',
-      'pervers',
-    ],
-    [
-      'illuminée',
-      'illuminé',
-    ],
-    [
-      'violente',
-      'violent',
-    ],
-    [
-      'rappeuse',
-      'rappeur',
-    ],
-    [
-      'gothique',
-      'gothique',
-    ],
-    [
-      'idiote',
-      'idiot',
-    ],
-    [
-      'limitée',
-      'limité',
-    ],
+    suffix: [
+      `General`,
+      `Bros`,
+      `Brawl`,
+      `Black Ops`,
+      `Battlegrounds`,
+      `Tycoon`,
+      `Simulator`,
+      `Reloaded`,
+      `GOTY`,
+      `(beta)`,
+      `Bannerlord`,
+      `Fortress`,
+      `Craft`,
+      `Ultimate`,
+      `II Turbo alpha`
+    ]
+  },
+  gamerTags: [
+    "Box",
+    "Beat",
+    "Flow",
+    "Great",
+    "Heavy",
+    "Fat",
+    "1998",
+    "French",
+    "Cock",
+    "Pompon",
+    "Weiner",
+    "Sugar",
+    "Mom",
+    "Daddy",
+    "_666_",
+    "Dark",
+    "Fucker",
+    "Sancho",
+    "Blob",
+    "Marvel",
+    "Heat",
+    "Feet"
   ]
-}
+};
